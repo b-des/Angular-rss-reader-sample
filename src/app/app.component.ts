e84bc1d8ad4eec491ef5d3bd1097c8fb3036fcff
b-des <alltelex@gmail.com>
2019-07-04 19:18:52 +0300

    Refactored code with routes and backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {RssLinkModel} from './core/models/rss-link.model';
 import {RssService} from './core/services/rss.service';
-import {from} from 'rxjs';
-import {filter, map, toArray} from 'rxjs/operators';
-import {LOCAL_STORAGE, WebStorageService} from 'angular-webstorage-service';
-import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
-import {NgxXml2jsonService} from 'ngx-xml2json';
-import {NotificationModel, RssFeedModel} from './core/models';
+
 
 @Component({
     selector: 'app-root',
